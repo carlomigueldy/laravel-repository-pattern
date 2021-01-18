777465d699a409d71ac783314d08bc8965186855
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-01-18 16:01:26 +0800

    created UserController and a single GET endpoint for testing

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+$router->get('users', [UserController::class, 'index']);
