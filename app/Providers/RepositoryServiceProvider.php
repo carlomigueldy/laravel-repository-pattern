74cd7801f6c16d2a7253cb5744aebaf28ded1cc0
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-01-18 16:00:51 +0800

    created RepositoryServiceProvider and registered the binded the repositories inside it and have registered the RepositoryServiceProvider inside AppServiceProvider

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repository\Eloquent\BaseRepository;
+use App\Repository\Eloquent\UserRepository;
+use App\Repository\EloquentRepositoryInterface;
+use App\Repository\UserRepositoryInterface;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(EloquentRepositoryInterface::class, BaseRepository::class);
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
