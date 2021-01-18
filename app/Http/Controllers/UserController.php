e1468e7901b78660aea3db3c1766af656a6fa039
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-01-18 16:21:11 +0800

    done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Repository\UserRepositoryInterface;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    private $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function index()
+    {
+        return response()->json($this->userRepository->all(), 200);
+    }
 }
