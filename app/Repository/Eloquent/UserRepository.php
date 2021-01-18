41c80027b3a9d6b3b0176d2a76bc50db8f35fa34
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-01-18 16:01:09 +0800

    created a UserRepository and UserRepositoryInterface extending BaseRepository

---
+++ app/Repository/Eloquent/UserRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository\Eloquent;
+
+use App\Models\User;
+use App\Repository\UserRepositoryInterface;
+
+class UserRepository extends BaseRepository implements UserRepositoryInterface
+{
+    /**
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * BaseRepository constructor.
+     *
+     * @param Model $model
+     */
+    public function __construct(User $model)
+    {
+        $this->model = $model;
+    }
+}
---
+++ app/Repository/UserRepositoryInterface.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace App\Repository;
+
+interface UserRepositoryInterface extends EloquentRepositoryInterface {}
